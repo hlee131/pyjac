9449b308799f271fc686266b9bafe05ac09505db
hlee131 <59949027+hlee131@users.noreply.github.com>
2022-12-02 16:52:32 -0800

    fix: fixed build script to put llvm flags first

--- build/build.sh
+++ build/build.sh
@@ -1,3 +1,3 @@
-mv *.o build 
-c++ `llvm-config --cxxflags --ldflags --libs core executionengine mcjit interpreter analysis native bitwriter --system-libs` \
-build/compiler.o build/lexer.o build/ast.o build/parser.o build/list.o build/utils.o build/symtab.o build/codegen.o -o $1 
+mv *.o build
+c++ build/compiler.o build/lexer.o build/ast.o build/parser.o build/list.o build/utils.o build/symtab.o build/codegen.o \
+`llvm-config --cxxflags --ldflags --libs core executionengine mcjit interpreter analysis native bitwriter --system-libs` -o $1
